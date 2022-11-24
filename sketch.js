9a688d79f1e345a0e0aa7a884c0d1640d17e66b5
Anya <108478328+marifrancomarques@users.noreply.github.com>
2022-11-24 11:22:44 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,50 +1,137 @@
-var database;
-var back_img;
-var gameState =0;
-var playerCount = 0;
-var allPlayers;
 var score =0;
-var player, form,game;
-var player1,player2;
-var players;
-var fruits;
-var fruitGroup;
-var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
-var player_img;
-var player1score =0;
-var player2score =0;
+var gun,bluebubble,redbubble, bullet, backBoard;
+
+var gunImg,bubbleImg, bulletImg, blastImg, backBoardImg;
+
+var redBubbleGroup, redBubbleGroup, bulletGroup;
+
+
+var life =3;
+var score=0;
+var gameState=1
 
 function preload(){
-  back_img = loadImage("images/jungle.jpg");
-  player_img = loadImage("images/basket2.png");
-  fruit1_img = loadImage("images/apple2.png");
-  fruit2_img = loadImage("images/banana2.png");
-  fruit3_img = loadImage("images/melon2.png");
-  fruit4_img = loadImage("images/orange2.png");
-  fruit5_img = loadImage("images/pineapple2.png");
-  fruitGroup = new Group();
+  gunImg = loadImage("gun1.png")
+  blastImg = loadImage("blast.png")
+  bulletImg = loadImage("bullet1.png")
+  blueBubbleImg = loadImage("waterBubble.png")
+  redBubbleImg = loadImage("redbubble.png")
+  backBoardImg= loadImage("back.jpg")
 }
 function setup() {
-  createCanvas(1000, 600);
-  database = firebase.database();
-  game = new Game();
-  game.getState();
-  game.start();
+  createCanvas(800, 600);
+
+  backBoard= createSprite(50, width/2, 100,height);
+  backBoard.addImage(backBoardImg)
   
+  gun= createSprite(100, height/2, 50,50);
+  gun.addImage(gunImg)
+  gun.scale=0.2
+  
+  bulletGroup = createGroup();   
+  blueBubbleGroup = createGroup();   
+  redBubbleGroup = createGroup();   
+  
+  heading= createElement("h1");
+  scoreboard= createElement("h1");
 }
 
 function draw() {
-  background(back_img);
+  background("#BDA297");
   
-   if (playerCount === 2) {
-     game.update(1);
-   }
-   if (gameState === 1) {
-     clear(); 
-     game.play();
-   }
-   if (gameState === 2) {
+  heading.html("Vida: "+life)
+  heading.style('color:red'); 
+  heading.position(150,20)
+
+  scoreboard.html("Pontuação: "+score)
+  scoreboard.style('color:red'); 
+  scoreboard.position(width-200,20)
+
+  if(gameState===1){
+    gun.y=mouseY  
+
+    if (frameCount % 80 === 0) {
+      drawblueBubble();
+    }
+
+    if (frameCount % 100 === 0) {
+      drawredBubble();
+    }
+
+    if(keyDown("space")){
+      shootBullet();
+    }
+
+    if (blueBubbleGroup.collide(backBoard)){
+      handleGameover(blueBubbleGroup);
+    }
+    if (redBubbleGroup.collide(backBoard)) {
+      handleGameover(redBubbleGroup);
+    }
     
-     game.end();
-   }
+    if(blueBubbleGroup.collide(bulletGroup)){
+      handleBubbleCollision(blueBubbleGroup);
+    }
+
+    if(redBubbleGroup.collide(bulletGroup)){
+      handleBubbleCollision(redBubbleGroup);
+    }
+
+    drawSprites();
+  }
+    
+  
+}
+
+function drawblueBubble(){
+  bluebubble = createSprite(800,random(20,780),40,40);
+  bluebubble.addImage(blueBubbleImg);
+  bluebubble.scale = 0.1;
+  bluebubble.velocityX = -8;
+  bluebubble.lifetime = 400;
+  blueBubbleGroup.add(bluebubble);
+}
+function drawredBubble(){
+  redbubble = createSprite(800,random(20,780),40,40);
+  redbubble.addImage(redBubbleImg);
+  redbubble.scale = 0.1;
+  redbubble.velocityX = -8;
+  redbubble.lifetime = 400;
+  redBubbleGroup.add(redbubble);
+}
+
+function shootBullet(){
+  bullet= createSprite(150, width/2, 50,20)
+  bullet.y= gun.y-20
+  bullet.addImage(bulletImg)
+  bullet.scale=0.12
+  bullet.velocityX= 7
+  bulletGroup.add(bullet)
+}
+
+function handleBubbleCollision(bubbleGroup){
+    if (life > 0) {
+       score=score+1;
+    }
+
+    blast= createSprite(bullet.x+60, bullet.y, 50,50);
+ blast.addImage(blastImg)
+    blast.scale=0.3
+    blast.life=20
+    bulletGroup.destroyEach()
+    bubbleGroup.destroyEach()
+}
+
+function handleGameover(bubbleGroup){
+  
+    life=life-1;
+    bubbleGroup.destroyEach();
+    
+
+    if (life === 0) {
+      gameState=2
+      
+     
+    }
+  
 }
